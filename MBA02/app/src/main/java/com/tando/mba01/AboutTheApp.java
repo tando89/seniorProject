86b57f56ba54d8de513de2a30d59fca72acf48c9
tando89 <tantdo89@gmail.com>
2017-06-10 15:18:03 -0700

    finish v3.0

--- MBA02/app/src/main/java/com/tando/mba01/AboutTheApp.java
+++ MBA02/app/src/main/java/com/tando/mba01/AboutTheApp.java
@@ -7,17 +7,10 @@ import android.view.View;
 import android.widget.ListView;
 
 public class AboutTheApp extends AppCompatActivity {
-    ListView iconLv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_the_app);
-
-        iconLv = (ListView) findViewById(R.id.iconListView);
-
-        customizedAboutApp adapter = new customizedAboutApp(AboutTheApp.this);
-
-        iconLv.setAdapter(adapter);
     }
     //back to homepage when click the logo
     public void backHome (View home) {
